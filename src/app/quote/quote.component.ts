2832027b01faa0436210d130bbf8c411860708fe
Chris-karanja <chriswainaina0@gmail.com>
2018-11-28 22:58:29 +0300

    testing array userquotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,6 +8,14 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
+  userQuotes = [
+    new Quote(1, 'Quote number 1', 'kay', 0, 0, new Date(2018, 7, 10)),
+    new Quote(2, 'Quote number 2', 'king', 0, 0, new Date(2018, 7, 10)),
+    new Quote(3, 'Quote number 3', 'lucy', 0, 0, new Date(2018, 7, 10)),
+    new Quote(4, 'Quote number 4', 'kena', 0, 0, new Date(2018, 7, 10)),
+    new Quote(5, 'Quote number 5', 'Weshy', 0, 0, new Date(2018, 7, 10))
+  ];
+
   constructor() { }
 
   ngOnInit() {
