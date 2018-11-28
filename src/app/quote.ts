533ed99665969351c2d6a88f4dcdaaec4ffef423
Chris-karanja <chriswainaina0@gmail.com>
2018-11-28 23:37:49 +0300

    tooglefuntionality added

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,7 @@
 export class Quote {
-    constructor(public quoteID: number, public message: string, public author: string,
-         public upVote: number, public downVote: number, public dateAdded: Date) { }
+    public showDetail: boolean;
+    constructor(public quoteID: number, public message: string, public author: string, public upVote: number, public downVote
+        : number, public dateAdded: Date) {
+        this.showDetail = false;
+    }
 }
